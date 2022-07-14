a7a8539753047100ed4bd79a6ab88ff667a0d5bc
lily08 <liliangelxd@gmail.com>
2022-07-14 13:32:39 -0400

    complete lab

--- app.js
+++ app.js
@@ -2,9 +2,33 @@ const express = require('express')
 
 const app = express()
 
+const locationController = require('../express-fitness/controllers/locations.controller')
+
+const personController = require('../express-fitness/controllers/persons.controller')
+
+const planController = require('../express-fitness/controllers/plans.controller')
+
+const machineController = require('../express-fitness/controllers/machines.controller')
+
+const specialEventsController = require('../express-fitness/controllers/special-events.controller')
+
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.send('Hello, world!')
+})
+
+app.use('/locations', locationController)
+
+app.use('/persons', personController)
+
+app.use('/plans',  planController)
+
+app.use('/machines', machineController)
+
+app.use('/special-events', specialEventsController)
+
+app.use('*', (req, res) => {
+  res.status(404).send('Oops! Nothing to see here')
 })
 
 module.exports = app
