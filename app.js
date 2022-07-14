a2dc084dca77043a3cb914c6303c67dc00037e26
Charlie Moran <Carlosamoran18@gmail.com>
2022-07-14 18:52:37 -0400

    app fix

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+const locationController = require('../express-fitness/controllers/locations.controller')
+const personController = require('../express-fitness/controllers/persons.controller')
+const planController = require('../express-fitness/controllers/plans.controller')
+const machineController = require('../express-fitness/controllers/machines.controller')
+const specialEventsController = require('../express-fitness/controllers/special-events.controller')
 
-require("dotenv").config();
-
-app.get("/", (req, res) => {
-    res.send("Hello, world!")
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
 })
+
+app.use('/locations', locationController)
+
+app.use('/persons', personController)
+
+app.use('/plans',  planController)
+
+app.use('/machines', machineController)
+
+app.use('/special-events', specialEventsController)
+
+app.use('*', (req, res) => {
+  res.status(404).send('Oops! Nothing to see here')
+})
+
+module.exports = app
