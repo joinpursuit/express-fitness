17f1b50d0c6dbae40a5cc5154729d5c3ff6e933e
meera-ramesh19 <meeraramesh19@outlook.com>
2022-07-14 14:18:27 -0400

    bonus completed but the location value is wrong in the array

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const locations = express.Router();
 const locationsArray = require('../models/location.model.js');
+const peoplesArray = require('../models/person.model.js');
 
 locations.get('/', (req, res) => {
   res.json(locationsArray);
+});
+
+locations.get('/people', (req, res) => {
+  let peopleInLocation = locationsArray.map((location) => {
+    let peopleFiltered = peoplesArray.filter((person) => {
+      return person.mainLocation === location.zip;
+    });
+    location.people = peopleFiltered;
+    return location;
+  });
+  res.json(peopleInLocation);
 });
 
 module.exports = locations;
