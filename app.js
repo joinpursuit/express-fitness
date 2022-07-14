d14dffa69ce05f6c98c246e0d363f6594e5ce154
EmaleeSoto <emaleesoto@gmail.com>
2022-07-14 12:52:30 -0400

    Added controller routes and use, passes all tests

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+const locationsController = require("./controllers/locations.controller.js");
+app.use("/locations", locationsController);
+const machinesController = require("./controllers/machines.controller.js");
+app.use("/machines", machinesController);
+const personsController = require("./controllers/persons.controller.js");
+app.use("/persons", personsController);
+const plansController = require("./controllers/plans.controller.js");
+app.use("/plans", plansController);
+const specialEventsController = require("./controllers/special-events.controller.js");
+app.use("/special-events", specialEventsController);
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
