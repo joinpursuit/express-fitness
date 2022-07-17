58c52db108e4d7eb1a0ba8fa34ddb3389dd8b99a
Victor E Lopez <velbx51594@macbook-air.mynetworksettings.com>
2022-07-17 12:28:13 -0400

    express-fitness-lab

---
+++ controllers/special-events.controller.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const specialEvents = express.Router();
+
+const specialEventsData = require("../models/special-event.model.js");
+
+specialEvents.get("/", (req, res) => {
+    res.json(specialEventsData)
+});
+
+
+
+module.exports = specialEvents
