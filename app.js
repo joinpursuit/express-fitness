c6cb2d03d6b0377176494ae60c1a33fe3f6e62da
Dannell <dannellmorales@pursuit.org>
2022-07-17 17:11:22 -0400

    Ametza/Dan Express Fitness Complete

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+const locationsController = require("./controllers/locations.controller.js");
+const machinesController = require("./controllers/machines.controller.js");
+const personsController = require("./controllers/persons.controller.js");
+const plansController = require("./controllers/plans.controller.js");
+const specialController = require("./controllers/special-events.controller.js");
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!")
+});
+
+ app.use("/locations", locationsController);
+ app.use("/machines", machinesController);
+ app.use("/persons", personsController);
+ app.use("/plans", plansController);
+ app.use("/special-events", specialController);
+
+app.get("*", (req, res) => {
+    res.status(404).json({error: "page not found"})
+})
+
+module.exports = app;
