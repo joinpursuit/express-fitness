b33b572202d6e3b31d5517106d6d172ca5795009
Tom LaTulipe <tomlatulipe@pursuit.org>
2022-07-17 12:55:14 -0400

    Everything passing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const locationController = require("./controllers/locations.controller");
+const locationsController = require("./controllers/locations.controller");
 const machineController = require("./controllers/machines.controller");
 const personController = require("./controllers/persons.controller");
 const eventController = require("./controllers/special-events.controller");
@@ -11,10 +11,14 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.use("/locations", locationController);
-app.use("/equipment", machineController);
-app.use("/people", personController);
+app.use("/locations", locationsController);
+app.use("/machines", machineController);
+app.use("/persons", personController);
 app.use("/special-events", eventController);
 app.use("/plans", planController);
+
+app.get("*", (req, res) => {
+    res.status(404).json({error: "page not found"})
+})
 
 module.exports = app;
