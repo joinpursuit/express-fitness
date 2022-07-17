2c0d4921a28feeecf68dd06c76c5447769277058
Mason Mei <masonmei@pursuit.org>
2022-07-17 12:55:30 -0400

    added /locations/people

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -2,18 +2,14 @@ const express = require("express")
 const locations = express.Router();
 
 const locationsData = require("../models/location.model.js")
+const peopleData = require("../models/people.model.js")
 
 locations.get("/", (req, res) => {
     res.json(locationsData)
 })
 
-// locations.get("/:id", (req, res) => {
-//     const { id } =  req.params;
-//     if(locationsData[id]) {
-//         res.json(locationsData[id])
-//     } else {
-//         res.status(404).send("no bookmark found - sorry")
-//     }
-// })
+locations.get("/people", (req, res) => {
+        res.json(peopleData)
+})
 
 module.exports = locations
